09bdad3c48eb99382d553d210060ac63a751a4a1
Shahbazi <Shahbazi@DESKTOP-BN62A5G>
2023-10-22 14:42:51 +0330

    change daaaaaaa

--- WebFramework/MongoDBConfig.cs
+++ WebFramework/MongoDBConfig.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 public static class MongoDBConfig
 {
     public static void InitMongo(this IServiceCollection services)
-    {
+    { 
         var mongoClient = new MongoClient("mongodb://localhost:27017");
         var mongoDatabase = mongoClient.GetDatabase("CQRSDB");
        // services.AddSingleton<ICaptchaRepositories, CaptchaRepositories>();
